547bdb2b15850e1e6b2f10742529bfd313f31e33
Suhith Rajesh <10973031+suhithr@users.noreply.github.com>
2025-01-22 14:48:26 -0500

    Adds a profile utility

---
+++ profile_function.h
@@ -0,0 +1,21 @@
+/* Profiling Function */
+#ifndef PROFILE_FUNCTION_H
+#define PROFILE_FUNCTION_H
+
+#include <iostream>
+#include <chrono>
+#include <utility>
+
+template <typename Func, typename... Args>
+void profileFunction(const std::string& name, Func&& func, Args&&... args)
+{
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end_time - start_time;
+    std::cout << "Function [" << name << "] took " << duration.count() << " ms.\n";
+}
+
+#endif
